d0cd1ecb4c79c21c5ace8d224951c635c5716b01
btc4968 <donaldo11sea@gmail.com>
2026-05-04 02:56:30 -0700

    some ui updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-// import HomePage from "./pages/HomePage";
-// import ProductPage from "./pages/ProductPage";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-sky-50 via-indigo-50/40 to-white text-slate-900">
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        {/* <Route path="/products/:productId" element={<ProductPage />} /> */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products/:productId" element={<ProductPage />} />
       </Routes>
       <Footer />
     </div>
